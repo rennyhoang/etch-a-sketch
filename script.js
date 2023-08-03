97ac253c6f863048af55dfedc8101f1785a9c163
Renny Hoang <renny@rennyhoang.com>
2023-08-03 18:19:43 -0500

    implemented opacity increment

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 const sketchBody = document.querySelector('body');
 
+const userInput = document.createElement('button');
+userInput.textContent = "choose sketch size";
+sketchBody.appendChild(userInput);
+
 const sketchGrid = document.createElement('div');
 sketchGrid.id = "container";
 
-const GRID_LENGTH = 16;
-const CHILD_LENGTH = 100 / (GRID_LENGTH + 2);
+userInput.addEventListener('mousedown', () => { 
+    const GRID_LENGTH = getUserInput(); 
+    const CHILD_LENGTH = 100 / (GRID_LENGTH);
+    sketchGrid.innerHTML = '';  
+    drawGrid(GRID_LENGTH, CHILD_LENGTH);
+});
 
-function drawGrid(){
+function getUserInput(){
+    let inputValid = false;
+    let res = "";
+    while(!inputValid){
+        const currInput = prompt("choose sketch size");
+        if(isNaN(currInput)){
+            continue;
+        }
+        if(parseInt(currInput) < 1){
+            continue;
+        }
+        res = parseInt(currInput);
+        inputValid = true;
+    }
+
+    return res;
+}
+
+function drawGrid(GRID_LENGTH = 16, CHILD_LENGTH = 100 / 16){
     sketchBody.appendChild(sketchGrid);
     for(i = 0; i < GRID_LENGTH; i++){
         for(j = 0; j < GRID_LENGTH; j++){
@@ -14,7 +40,12 @@ function drawGrid(){
             const currChild = document.createElement('div');
             currChild.classList.add('child');
             currChild.style.flexBasis = CHILD_LENGTH + "%";
-            currChild.addEventListener('mouseenter', () => {currChild.style.backgroundColor = "#000000"; })
+            currChild.addEventListener('mouseenter', () => {
+                currChild.style.backgroundColor = "#000000"; 
+                let opacity = parseFloat(currChild.style.opacity) || 0;
+                opacity += .1;
+                currChild.style.opacity = opacity;
+            })
             sketchGrid.appendChild(currChild);
         }
     }
